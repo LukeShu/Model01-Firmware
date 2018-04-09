28b43306c0071f1c606a01e85c34bbdbef8dd4fd
Luke Shumaker <lukeshu@lukeshu.com>
2018-04-08 21:09:11 -0400

    use those shorter key aliases
      Key_Left     -> Key_L (except for Key_LeftArrow)
      Key_Right    -> Key_R (except for Key_RightArrow)
      Key_Keypad   -> Key_KP_
      CurlyBracket -> Brace
      Multiply     -> Mul
      Subtract     -> Sub
      Divide       -> Div
      Control      -> Ctrl
      Spacebar     -> Space
      Escape       -> Esc
      Period       -> Dot
      Semicolon    -> Semi

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -143,15 +143,15 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G, /* skip */
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Esc,
+   Key_LCtrl, Key_Backspace, Key_LGui, Key_LShift,
    ShiftToLayer(FUNCTION),
 
    M(M_ANY),      Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-   /* skip */     Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   /* skip */     Key_H, Key_J, Key_K,     Key_L,         Key_Semi,      Key_Quote,
+   Key_RAlt,      Key_N, Key_M, Key_Comma, Key_Dot,       Key_Slash,     Key_Minus,
+   Key_RShift, Key_LAlt, Key_Space, Key_RCtrl,
    ShiftToLayer(FUNCTION)),
 
 
@@ -163,10 +163,10 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ___, ___, ___, ___,
    ___,
 
-   M(M_INFO),              ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___,
-   ___,                    ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___,
-   /* skip */              ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         Key_Quote,
-   ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
+   M(M_INFO),              ___, Key_KP_7, Key_KP_8,   Key_KP_9,        Key_KP_Sub,      ___,
+   ___,                    ___, Key_KP_4, Key_KP_5,   Key_KP_6,        Key_KP_Add,      ___,
+   /* skip */              ___, Key_KP_1, Key_KP_2,   Key_KP_3,        Key_Equals,      Key_Quote,
+   ___,                    ___, Key_KP_0, Key_KP_Dot, Key_KP_Mul,      Key_KP_Div,      Key_Enter,
    ___, ___, ___, ___,
    ___),
 
@@ -179,7 +179,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
+   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LBrace,               Key_RBrace,               Key_LBracket,    Key_RBracket,     Key_F12,
    /* skip */                  Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
    Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
    ___, ___, Key_Enter, ___,
